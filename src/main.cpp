0310b1fc4733cf1aaed234936a29e667cc4aa6ba
Nicholas Spihlman <nspihlman@gmail.com>
2025-11-03 11:50:33 -0500

    Moving parser to own file

--- src/main.cpp
+++ src/main.cpp
@@ -13,38 +13,8 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include "user_set_value.h"
+#include "parser.h"
 
-
-std::string parse_string(const char*& buffer){
-  //string_start is the index in the buffer that the $ is found
-  char* end;
-  long string_length = strtol(buffer+1, &end, 10);
-  const char* stringStart = end + 2;
-  buffer = stringStart + string_length + 2; // now buffer is pointing to the next $ if there is one, or a nullptr
-  return std::string(stringStart, string_length);
-}
-
-std::vector<std::string> parse_array(const char*& buffer, size_t length){
-  char* end;
-  std::vector<std::string> arrayValues;
-  long array_len = std::strtol(buffer+1, &end, 10);
-  buffer = end + 2; // buffer now points to the first $
-  while(array_len > 0){
-    --array_len;
-    arrayValues.push_back(parse_string(buffer));
-  }
-  return arrayValues;
-}
-
-std::vector<std::string> parser(const char*& buffer, size_t length){
-  if (buffer[0] == '*'){
-    return parse_array(buffer, length);
-  }
-  if (buffer[0] == '$'){
-    return {parse_string(buffer)};
-  }
-  return {""};
-}
 
 void process_set_message(int client_fd, std::vector<std::string>& commands, std::unordered_map<std::string, UserSetValue>& user_set_values){
   UserSetValue newValue = UserSetValue(commands[2]);
---
+++ src/parser.h
@@ -0,0 +1,8 @@
+#include <string>
+
+std::string parse_string(const char*& buffer);
+
+std::vector<std::string> parse_array(const char*& buffer, size_t length);
+
+std::vector<std::string> parser(const char*& buffer, size_t length);
+
