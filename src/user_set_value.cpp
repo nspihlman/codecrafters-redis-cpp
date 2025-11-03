380a26009a8df5f25b032cf99029c3516876e297
Nicholas Spihlman <nspihlman@gmail.com>
2025-11-03 09:10:29 -0500

    Fix cpp

--- src/user_set_value.cpp
+++ src/user_set_value.cpp
@@ -1,19 +1,13 @@
 #include "user_set_value.h"
 
-class UserSetValue{
-public:
-  std::string value;
-  int64_t msExpiry = -1; // expiry time in milliseconds. -1 denotes a UserSetValue with no expiry defined
-  std::chrono::steady_clock::time_point inserted = std::chrono::steady_clock::now();
 
-  UserSetValue() : value("") {}
-  UserSetValue(std::string value) : value(value) {}
-  UserSetValue(std::string value, int64_t msExpiry) : value(value), msExpiry(msExpiry) {}
+UserSetValue::UserSetValue() : value("") {}
+UserSetValue::UserSetValue(std::string value) : value(value) {}
+UserSetValue::UserSetValue(std::string value, int64_t msExpiry) : value(value), msExpiry(msExpiry) {}
 
-  bool stillValid(){
+bool UserSetValue::stillValid(){
     if(msExpiry == -1){
-      return true;
+        return true;
     }
     return (std::chrono::steady_clock::now() - inserted) < std::chrono::milliseconds(msExpiry);
-  }
-};
+}
